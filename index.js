afa7a71ed94b6b263e504c79cb1067821590b2a2
appsmanage <84610397+appsmanage@users.noreply.github.com>
2021-05-22 18:25:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ $(function () {
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
         
-        //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg = `${key}\n理由：${reasons}`;
+        //var msg = `${key}\nお問合せ内容：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
+        var msg = `${key}\n内容：${reasons}`;
         sendText(msg);
 
         return false;
